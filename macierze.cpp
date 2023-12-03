0b5262dc84b732591c7e604c13598ec5ca7f1d8f
azatal <lukasz.stodolka100@gmail.com>
2023-12-03 02:19:37 +0100

    tak

--- macierze.cpp
+++ macierze.cpp
@@ -1,55 +1,104 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+typedef long long ll;
+
 struct Matrix {
-    std::vector<std::vector<int>> data;
-    int rows;
-    int cols;
-
-    // Constructor to initialize the matrix
-    Matrix(int rows, int cols) : rows(rows), cols(cols) {
-        data.resize(rows, std::vector<int>(cols, 0));
-    }
-
-    // Operator * to perform matrix multiplication
-    Matrix operator*(const Matrix& other) {
-        Matrix result(rows, other.cols);
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < other.cols; j++) {
-                for (int k = 0; k < cols; k++) {
-                    result.data[i][j] += data[i][k] * other.data[k][j];
-                }
+    ll n; 
+    vector<vector<ll>> ele;
+    Matrix(ll a, bool xd) {
+        n = a;
+        ele = vector<vector<ll>> (a);
+        for(ll i=0; i<a; i++) {
+            for(ll j=0; j<a; j++) {
+                ele[i].push_back({});
+                ele[i][j] = 0;
             }
         }
-
+        
+    }
+    Matrix(ll a) {
+        n = a;
+       ele = vector<vector<ll>> (a);
+        for(ll i=0; i<a; i++) {
+            for(ll j=0; j<a; j++) {
+                ele[i].push_back({});
+            }
+        }
+        for(int i=0; i<a; i++) {
+            ele[i][i] = 1;
+        }
+    }
+    Matrix(ll a, vector<ll> elements) {
+       n = a;
+       ele = vector<vector<ll>> (a);
+       ll k = 0;
+       for(ll i=0; i<a; i++) {
+           for(ll j=0; j<a; j++) {
+                ele[i].push_back(elements[k]);
+                k++;
+           }
+       }
+    }
+   
+    Matrix operator*(Matrix another) {
+        Matrix result(n, false);
+        for(ll i=0; i<n; i++) {
+            for(ll j=0; j<n; j++) {
+                for(ll k=0; k<n; k++) {
+                    result.ele[i][j] += ele[i][k] * another.ele[k][j];
+                }
+            }
+        } 
         return result;
     }
-};
 
-int main() {
-    // Example matrices using the Matrix struct
-    Matrix A(2, 2);
-    cout<<"Podaj macierz 1: "<<'\n';
-    int a, b, c, d;
-    cin>>a>>b>>c>>d;
-    A.data = {{a,b}, {c,d}};
-    cout<<"Podaj macierz 2: "<<'\n';
-    cin>>a>>b>>c>>d;
-    Matrix B(2, 2);
-    B.data = {{a,b}, {c, d}};
-
-    // Perform matrix multiplication using the operator *
-    Matrix result = A * B;
-
-    // Display the result
-    for (int i = 0; i < result.rows; i++) {
-        for (int j = 0; j < result.cols; j++) {
-            std::cout << result.data[i][j] << " ";
+    Matrix operator^(int expo) {
+        Matrix res(n);
+        Matrix base = *this;
+        while(expo > 0) {
+            if(expo % 2 == 1) res = res*base;
+            base = base * base;
+            expo/=2;
         }
-        std::cout << std::endl;
+        return res;
     }
 
-    return 0;
+};
+
+ostream& operator<<(ostream& os, const Matrix& matrix) {
+        for(ll i=0; i<matrix.n; i++) {
+            for(ll j=0; j<matrix.n; j++) {
+                os<<matrix.ele[i][j]<<' ';
+            }
+            os<<'\n';
+        }
+        return os;
 }
 
+Matrix fastExpo(ll n, Matrix base, ll expo) {
+    Matrix res(n);
+    while(expo > 0) {
+        if(expo % 2 == 1) {
+            res = (res * base);
+        }
+        base = (base * base);
+        expo/=2;
+    }
+
+    return res;
+}
+
+int main() {
+
+    ll n; cin>>n;
+    vector<ll> nums;
+    for(ll i=0; i<n; i++) {
+        for(ll j=0; j<n; j++) {
+            ll a; cin>>a;
+            nums.push_back(a);
+        }
+    }
+    Matrix macierz(n, nums);
+    cout<<macierz*macierz;
+}
